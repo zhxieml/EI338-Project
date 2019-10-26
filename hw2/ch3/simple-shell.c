3e7d0e67536e28a6545b782e9d15faf9d215f1dc
fffffarmer <fffffarmer@sjtu.edu.cn>
2019-10-26 18:23:14 +0800

    update

--- hw2/ch3/simple-shell.c
+++ hw2/ch3/simple-shell.c
@@ -11,37 +11,50 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <fcntl.h>
 
 #define MAX_LINE		80 /* 80 chars per line, per command */
 
-int parse_cmd(char *cmd, char **args) {
+int parse_cmd(char *cmd, char **args, char *ifile, char *ofile) {
 	char *token;
 	int i = 0;
+	char redirect;
 	
-	token = strtok(cmd, " ");
+	token = strtok(cmd, " "); /* take the first token */
 
 	while (token) {
-		printf("%s\n", token);
+		if (!strcmp(token, "<") || !strcmp(token, ">")) { /* redirect */
+			redirect = token[0];
+		}
+		else {
+			switch (redirect)
+			{
+			case '<':
+				strcpy(ifile, token);
+				break;
 
-		args[i] = (char*) malloc(sizeof(char) * (strlen(token)));
-		
-		strcpy(args[i], token);
+			case '>':
+				strcpy(ofile, token);
+				break;
+			
+			default:
+				args[i] = (char*) malloc(sizeof(char) * (strlen(token)));
+				strcpy(args[i], token);
+				++i;
+			}
+		}
 
-		++i;
-
-		token = strtok(NULL, " ");
+		token = strtok(NULL, " "); /* take the next token */
 	}
-
-	printf("Last arg is %s\n", args[i - 1]);
 
 	if (!strcmp(args[i - 1], "&")) 
 	{
-		args[i - 1] = NULL;
+		args[i - 1] = NULL; /* clear '$' */
 	
-		return 0;
+		return 0; /* wait flag is set to 0 (concurrent) */
 	}
 	
-	return 1;
+	return 1; /* wait flag is set to 1 */
 }
 
 void print_args(char **args) {
@@ -54,17 +67,45 @@ void print_args(char **args) {
 	}
 }
 
-int exe(char **args, int wait_flag) {
+int exe(char **args, int wait_flag, char *ifile, char *ofile) {
 	pid_t pid;
+	off_t fd;
+	int exe_err;
 
 	pid = fork();
 
 	if (pid < 0) { /* error occurred */
-		fprintf(stderr, "Fork Failed");
+		fprintf(stderr, "Fork Failed\n");
 		return 1;
 	}
 	else if (pid == 0) { /* child process */
-		execvp(args[0], args);
+		if (strlen(ifile)) { // TODO: sort < input.txt
+			fd = open(ifile, O_RDWR);
+			
+			if (fd < 0) {
+				printf("Error occurs when opening %s\n", ifile);
+				exit(0);
+			}
+			
+			dup2(fd, STDOUT_FILENO);
+		}
+		else if (strlen(ofile))	
+		{
+			fd = open(ofile, O_RDWR|O_CREAT);
+
+			if (fd < 0) {
+				printf("Error occurs when opening %s\n", ofile);
+				exit(0);
+			}
+			
+			dup2(fd, STDOUT_FILENO);
+		}
+		
+		exe_err = execvp(args[0], args);
+
+		if (exe_err < 0) printf("The command is not executable.\n");
+
+		close(fd);
 	}
 	else { /* parent process */
 		if (wait_flag) wait(NULL);
@@ -76,23 +117,24 @@ int main(void)
 	char *args[MAX_LINE/2 + 1];	/* command line (of 80) has max of 40 arguments */
 	int should_run = 1;
 	char cmd[MAX_LINE];
+	char cmd_history[MAX_LINE]; /* history buffer */
 	int wait_flag;
 
+	char ifile[20];
+	char ofile[20];
+
 	for (int i = 0; i < MAX_LINE/2 + 1; ++i) args[i] = NULL;
+	cmd_history[0] = '\0';
+	ifile[0] = '\0';
+	ofile[0] = '\0';
 	
 	while (should_run) {   
 		printf("osh>");
 		fflush(stdout);
 
 		fgets(cmd, MAX_LINE, stdin);
-		cmd[strlen(cmd) - 1] = '\0';
+		cmd[strlen(cmd) - 1] = '\0'; /* delete line-feed */
 
-		wait_flag = parse_cmd(cmd, args);
-
-		print_args(args);
-
-		exe(args, wait_flag);
-		
 		/**
 		 * After reading user input, the steps are:
 		 * (1) fork a child process
@@ -100,7 +142,32 @@ int main(void)
 		 * (3) if command included &, parent will invoke wait()
 		 */
 
+		if (!strcmp(cmd, "!!")) {
+			if (strlen(cmd_history) == 0) 
+			{
+				printf("-----------------------\n");
+				printf("No commands in history.\n");
+				printf("-----------------------\n");
+			}
+			else {
+				printf("-------------------------------------------------\n");
+				printf("Command '%s' in the history will be executed.\n", cmd_history);
+				printf("-------------------------------------------------\n");
+
+				strcpy(cmd, cmd_history);
+			}
+		}	
+		else strcpy(cmd_history, cmd);
+
+		wait_flag = parse_cmd(cmd, args, ifile, ofile);
+
+		printf("Input file: %s; output file: %s\n", ifile, ofile);
+
+		exe(args, wait_flag, ifile, ofile);
+
 		for (int i = 0; i < MAX_LINE/2 + 1; ++i) args[i] = NULL;
+		ifile[0] = '\0';
+		ofile[0] = '\0';
 	}
     
 	return 0;
